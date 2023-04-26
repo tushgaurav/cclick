b5c067d7399ccb9ac15aa77c29aed4629fcda05e
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-04-26 15:55:25 +0530

    fixed qr code styling

--- components/QRCode.tsx
+++ components/QRCode.tsx
@@ -2,12 +2,18 @@
 
 import { useRef, useEffect } from "react";
 import { usePathname } from "next/navigation";
+import styles from "@/styles/qrcode.module.css";
 import options from "../public/misc/qr_options.js";
 
 const QRCode = () => {
   const pathname = usePathname();
 
   const ref = useRef(null);
+
+  const qrToggle = () => {
+    const qrCode = document.getElementById("qr-code");
+    qrCode?.classList.toggle(styles.qr_code_active);
+  };
 
   useEffect(() => {
     // Dynamically import qr-code-styling only client-side
@@ -24,8 +30,12 @@ const QRCode = () => {
   }, []);
 
   return (
-    <div>
-      <div ref={ref} id="qr-code"></div>
+    <div className={styles.qr_code}>
+      <h2 className={styles.qr_code_title}>
+        QR Code
+        <span onClick={qrToggle}> &#x21ba;</span>
+      </h2>
+      <div ref={ref} id="qr-code" className={styles.qr_code_hidden}></div>
     </div>
   );
 };
