32ba7562e5da959a40b7bf65f2e9c7334f3662a4
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-04-26 09:05:17 +0530

    changed constant emoji with a dynamic one
    - now the emoji at the create new paste page changes each time
    - instead of one emoji, now added 4 emojis

--- app/new/paste/page.tsx
+++ app/new/paste/page.tsx
@@ -1,5 +1,6 @@
 import PasteForm from "@/components/PasteForm";
 import { currentUser } from "@clerk/nextjs/app-beta";
+import randomEmoji from "@/lib/emoji";
 
 export const metadata = {
   title: "Create new Paste | cclick.click",
@@ -10,11 +11,15 @@ export const metadata = {
 
 export default async function Paste() {
   const user = await currentUser();
+  const emojiString =
+    randomEmoji() + randomEmoji() + randomEmoji() + randomEmoji();
 
   return (
     <div className="container">
       <h1>Create New Paste</h1>
-      <h4>Create a new paste as {user?.firstName}😁</h4>
+      <h4>
+        Create a new paste as {user?.firstName} {emojiString}
+      </h4>
       <PasteForm
         userId={user.id}
         userName={`${user?.firstName} ${user?.lastName}`}
