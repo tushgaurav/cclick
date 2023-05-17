e3802e6b5e4480a85c7a12d670f755277e9ee7de
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-05-17 18:32:06 +0530

    finished my content page

--- app/(common)/my/page.tsx
+++ app/(common)/my/page.tsx
@@ -2,6 +2,8 @@ import styles from "@/styles/my.module.css";
 import { db } from "@/lib/db";
 import { currentUser } from "@clerk/nextjs/app-beta";
 import Link from "next/link";
+import Button from "@/components/Button";
+import Section from "@/components/Section";
 
 async function pasteCount(userId) {
   return await db.paste.count({
@@ -45,20 +47,74 @@ async function linkViews(userId) {
   return aggregations._sum.views;
 }
 
+async function topPaste(userId) {
+  return await db.paste.findFirst({
+    where: {
+      ownerId: userId,
+    },
+    orderBy: {
+      views: "desc",
+    },
+  });
+}
+
+async function topLink(userId) {
+  return await db.link.findFirst({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      views: "desc",
+    },
+  });
+}
+
 export default async function My() {
   const user = await currentUser();
   const paste_count = await pasteCount(user.id);
   const paste_views = await totalPasteViews(user.id);
   const links = await linkShortned(user.id);
   const link_views = await linkViews(user.id);
+  const top_paste = await topPaste(user.id);
+  const top_link = await topLink(user.id);
 
   return (
-    <div>
-      <main>There are your contents -</main>
+    <div className="container">
+      <h1>
+        Welcome, {user?.firstName} {user?.lastName}!
+      </h1>
+
       <p>View the pastes and links that you have created in the past.</p>
 
+      <Section>
+        <div>
+          <h2>Your Top Paste</h2>
+
+          <Link href={`/paste/${top_paste?.slug}`}>
+            <div>
+              <h3>{top_paste?.name}</h3>
+              <p>{top_paste?.views} views</p>
+              <p>{top_paste?.content}</p>
+            </div>
+          </Link>
+        </div>
+      </Section>
+
+      <h3>View all your content</h3>
+
+      <div className="button-container">
+        <Button>
+          <Link href="/my/pastes">View Pastes</Link>
+        </Button>
+
+        <Button>
+          <Link href="/my/links">View Links</Link>
+        </Button>
+      </div>
+
       <div className="stats">
-        <h1>Stats</h1>
+        <h2>Stats</h2>
+        <p>See how your pastes and links are performing.</p>
         <div className={styles.card_container}>
           <div className={styles.stat_card}>
             <h1 className="number">{paste_count}</h1>
@@ -81,9 +137,6 @@ export default async function My() {
           </div>
         </div>
       </div>
-
-      <Link href="/my/pastes">View Pastes</Link>
-      <Link href="/my/links">View Links</Link>
     </div>
   );
 }
